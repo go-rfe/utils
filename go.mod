fb2495e666d884d53354ac6252b2d91c1d382ef6
Igor Tiunov <itiunov@openwaygroup.com>
2022-04-08 16:29:21 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-rfe/utils
+
+go 1.17
---
+++ luhn/luhn.go
@@ -0,0 +1,41 @@
+package luhn
+
+const (
+	luhnMultiplier = 10
+)
+
+// CalculateLuhn return the check number
+func CalculateLuhn(number int) int {
+	checkNumber := checksum(number)
+
+	if checkNumber == 0 {
+		return 0
+	}
+
+	return luhnMultiplier - checkNumber
+}
+
+// Valid check number is valid or not based on Luhn algorithm
+func Valid(number int) bool {
+	return (number%luhnMultiplier+checksum(number/luhnMultiplier))%luhnMultiplier == 0
+}
+
+func checksum(number int) int {
+	var luhn int
+
+	for i := 0; number > 0; i++ {
+		cur := number % luhnMultiplier
+
+		if i%2 == 0 { // even
+			cur *= 2
+			if cur > luhnMultiplier-1 {
+				cur = cur%luhnMultiplier + cur/luhnMultiplier
+			}
+		}
+
+		luhn += cur
+		number /= luhnMultiplier
+	}
+
+	return luhn % luhnMultiplier
+}
